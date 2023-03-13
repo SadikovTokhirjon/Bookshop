50042d3e8afea35ba96766e16e20fc19cc3cea1c
SadikovTokhirjon <116348575+SadikovTokhirjon@users.noreply.github.com>
2023-03-13 11:52:50 +0500

    ListView and DetailView
    Books app ga book.view  ListView va DetailView dan foydalandik

--- books/views.py
+++ books/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
+
 from .models import Book
 
+class ListView(ListView):
+    template_name='list.html'
+    queryset=Book.objects.all()
+    context_object_name='lists'
 
-class ListView(View):
-    def get(self,request):
-        lists=Book.objects.all()
-        return render(request,'list.html',{'lists':lists})
+# class ListView(View):
+#     def get(self,request):
+#         lists=Book.objects.all()
+#         return render(request,'list.html',{'lists':lists})
 
-class DetailsView(View):
-    def get(self,request,pk):
-        details=Book.objects.get(id=pk)
-        return render(request,'details.html',{'details':details})
+class DetailsView(DetailView):
+    template_name = 'details.html'
+    pk_url_kwarg = 'pk'
+    context_object_name = 'details'
+    model = Book
+
+
+# class DetailsView(View):
+#     def get(self,request,pk):
+#         details=Book.objects.get(id=pk)
+#         return render(request,'details.html',{'details':details})
