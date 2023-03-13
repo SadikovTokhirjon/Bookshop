ddbefbacc0a821db2e2c003f53b555cb566c0c2e
SadikovTokhirjon <116348575+SadikovTokhirjon@users.noreply.github.com>
2023-03-13 11:26:35 +0500

    crispy moduli
    crispy moduli o'rnatdim va htmllarga qo'shdim

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'books',
     'users',
 #     tashqi
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL='users.CustomUser'
 
